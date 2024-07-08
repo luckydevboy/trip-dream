03d85ca443de25067469c94aa2c1e931ddfa049a
Mohammad Reza Ghasemi <mo.ghasemi@sadad.co.ir>
2024-07-08 11:16:53 +0330

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts,css}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#18181b",
+        secondary: "#e7e4e4",
+      },
+    },
+  },
+  plugins: [],
+};
